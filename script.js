04fbce0ee1b423c6aa20ad179b49e058509a95fb
SushantShrestha12 <88120835+SushantShrestha12@users.noreply.github.com>
2021-09-12 12:45:11 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -8,260 +8,260 @@ var navLinks = document.getElementById("navLinks")
           navLinks.style.right = "-200px"; 
         }
 
-        // For toggle  
-        function toggleClock() {
-          var x = document.getElementById("myDIV");
-          var y = document.getElementById('clockButton');
-          var z = x.style.display;
+        // // For toggle  
+        // function toggleClock() {
+        //   var x = document.getElementById("myDIV");
+        //   var y = document.getElementById('clockButton');
+        //   var z = x.style.display;
 
-          if (z == "block") {
-            x.style.display = "none";
-            y.innerHTML = 'Show answer';
-          } 
+        //   if (z == "block") {
+        //     x.style.display = "none";
+        //     y.innerHTML = 'Show answer';
+        //   } 
 
-          else {
-            x.style.display = "block";
-            y.innerHTML = 'Hide answer';
-          }   
-        }
+        //   else {
+        //     x.style.display = "block";
+        //     y.innerHTML = 'Hide answer';
+        //   }   
+        // }
 
-        function toggleClock2() {
-          var x = document.getElementById("myDIV2");
-          var y = document.getElementById('clockButton2');
-          var z = x.style.display;
+        // function toggleClock2() {
+        //   var x = document.getElementById("myDIV2");
+        //   var y = document.getElementById('clockButton2');
+        //   var z = x.style.display;
 
-          if (z == "block") {
-            x.style.display = "none";
-            y.innerHTML = 'Show answer';
-          } 
+        //   if (z == "block") {
+        //     x.style.display = "none";
+        //     y.innerHTML = 'Show answer';
+        //   } 
 
-          else {
-            x.style.display = "block";
-            y.innerHTML = 'Hide answer';
-          } 
-        }
+        //   else {
+        //     x.style.display = "block";
+        //     y.innerHTML = 'Hide answer';
+        //   } 
+        // }
 
-        function toggleClock3() {
-          var x = document.getElementById("myDIV3");
-          var y = document.getElementById('clockButton3');
-          var z = x.style.display;
+        // function toggleClock3() {
+        //   var x = document.getElementById("myDIV3");
+        //   var y = document.getElementById('clockButton3');
+        //   var z = x.style.display;
 
-          if (z == "block") {
-            x.style.display = "none";
-            y.innerHTML = 'Show answer';
-          } 
+        //   if (z == "block") {
+        //     x.style.display = "none";
+        //     y.innerHTML = 'Show answer';
+        //   } 
 
-          else {
-            x.style.display = "block";
-            y.innerHTML = 'Hide answer';
-          } 
-        }
+        //   else {
+        //     x.style.display = "block";
+        //     y.innerHTML = 'Hide answer';
+        //   } 
+        // }
 
-        function toggleClock4() {
-          var x = document.getElementById("myDIV4");
-          var y = document.getElementById('clockButton4');
-          var z = x.style.display;
+        // function toggleClock4() {
+        //   var x = document.getElementById("myDIV4");
+        //   var y = document.getElementById('clockButton4');
+        //   var z = x.style.display;
 
-          if (z == "block") {
-            x.style.display = "none";
-            y.innerHTML = 'Show answer';
-          } 
+        //   if (z == "block") {
+        //     x.style.display = "none";
+        //     y.innerHTML = 'Show answer';
+        //   } 
 
-          else {
-            x.style.display = "block";
-            y.innerHTML = 'Hide answer';
-          } 
-        }
+        //   else {
+        //     x.style.display = "block";
+        //     y.innerHTML = 'Hide answer';
+        //   } 
+        // }
 
-        function toggleClock5() {
-          var x = document.getElementById("myDIV5");
-          var y = document.getElementById('clockButton5');
-          var z = x.style.display;
+        // function toggleClock5() {
+        //   var x = document.getElementById("myDIV5");
+        //   var y = document.getElementById('clockButton5');
+        //   var z = x.style.display;
 
-          if (z == "block") {
-            x.style.display = "none";
-            y.innerHTML = 'Show answer';
-          } 
+        //   if (z == "block") {
+        //     x.style.display = "none";
+        //     y.innerHTML = 'Show answer';
+        //   } 
 
-          else {
-            x.style.display = "block";
-            y.innerHTML = 'Hide answer';
-          } 
-        }
+        //   else {
+        //     x.style.display = "block";
+        //     y.innerHTML = 'Hide answer';
+        //   } 
+        // }
 
-        function toggleClock6() {
-          var x = document.getElementById("myDIV6");
-          var y = document.getElementById('clockButton6');
-          var z = x.style.display;
+        // function toggleClock6() {
+        //   var x = document.getElementById("myDIV6");
+        //   var y = document.getElementById('clockButton6');
+        //   var z = x.style.display;
 
-          if (z == "block") {
-            x.style.display = "none";
-            y.innerHTML = 'Show answer';
-          } 
+        //   if (z == "block") {
+        //     x.style.display = "none";
+        //     y.innerHTML = 'Show answer';
+        //   } 
 
-          else {
-            x.style.display = "block";
-            y.innerHTML = 'Hide answer';
-          } 
-        }
+        //   else {
+        //     x.style.display = "block";
+        //     y.innerHTML = 'Hide answer';
+        //   } 
+        // }
 
-        function toggleClock7() {
-          var x = document.getElementById("myDIV7");
-          var y = document.getElementById('clockButton7');
-          var z = x.style.display;
+        // function toggleClock7() {
+        //   var x = document.getElementById("myDIV7");
+        //   var y = document.getElementById('clockButton7');
+        //   var z = x.style.display;
 
-          if (z == "block") {
-            x.style.display = "none";
-            y.innerHTML = 'Show answer';
-          } 
+        //   if (z == "block") {
+        //     x.style.display = "none";
+        //     y.innerHTML = 'Show answer';
+        //   } 
 
-          else {
-            x.style.display = "block";
-            y.innerHTML = 'Hide answer';
-          } 
-        }
+        //   else {
+        //     x.style.display = "block";
+        //     y.innerHTML = 'Hide answer';
+        //   } 
+        // }
 
-        function toggleClock8() {
-          var x = document.getElementById("myDIV8");
-          var y = document.getElementById('clockButton8');
-          var z = x.style.display;
+        // function toggleClock8() {
+        //   var x = document.getElementById("myDIV8");
+        //   var y = document.getElementById('clockButton8');
+        //   var z = x.style.display;
 
-          if (z == "block") {
-            x.style.display = "none";
-            y.innerHTML = 'Show answer';
-          } 
+        //   if (z == "block") {
+        //     x.style.display = "none";
+        //     y.innerHTML = 'Show answer';
+        //   } 
 
-          else {
-            x.style.display = "block";
-            y.innerHTML = 'Hide answer';
-          } 
-        }
+        //   else {
+        //     x.style.display = "block";
+        //     y.innerHTML = 'Hide answer';
+        //   } 
+        // }
 
-        function toggleClock9() {
-          var x = document.getElementById("myDIV9");
-          var y = document.getElementById('clockButton9');
-          var z = x.style.display;
+        // function toggleClock9() {
+        //   var x = document.getElementById("myDIV9");
+        //   var y = document.getElementById('clockButton9');
+        //   var z = x.style.display;
 
-          if (z == "block") {
-            x.style.display = "none";
-            y.innerHTML = 'Show answer';
-          } 
+        //   if (z == "block") {
+        //     x.style.display = "none";
+        //     y.innerHTML = 'Show answer';
+        //   } 
 
-          else {
-            x.style.display = "block";
-            y.innerHTML = 'Hide answer';
-          } 
-        }
+        //   else {
+        //     x.style.display = "block";
+        //     y.innerHTML = 'Hide answer';
+        //   } 
+        // }
 
-        function toggleClock10() {
-          var x = document.getElementById("myDIV10");
-          var y = document.getElementById('clockButton10');
-          var z = x.style.display;
+        // function toggleClock10() {
+        //   var x = document.getElementById("myDIV10");
+        //   var y = document.getElementById('clockButton10');
+        //   var z = x.style.display;
 
-          if (z == "block") {
-            x.style.display = "none";
-            y.innerHTML = 'Show answer';
-          } 
+        //   if (z == "block") {
+        //     x.style.display = "none";
+        //     y.innerHTML = 'Show answer';
+        //   } 
 
-          else {
-            x.style.display = "block";
-            y.innerHTML = 'Hide answer';
-          } 
-        }
+        //   else {
+        //     x.style.display = "block";
+        //     y.innerHTML = 'Hide answer';
+        //   } 
+        // }
 
-        function toggleClock11() {
-          var x = document.getElementById("myDIV11");
-          var y = document.getElementById('clockButton11');
-          var z = x.style.display;
+        // function toggleClock11() {
+        //   var x = document.getElementById("myDIV11");
+        //   var y = document.getElementById('clockButton11');
+        //   var z = x.style.display;
 
-          if (z == "block") {
-            x.style.display = "none";
-            y.innerHTML = 'Show answer';
-          } 
+        //   if (z == "block") {
+        //     x.style.display = "none";
+        //     y.innerHTML = 'Show answer';
+        //   } 
 
-          else {
-            x.style.display = "block";
-            y.innerHTML = 'Hide answer';
-          } 
-        }
+        //   else {
+        //     x.style.display = "block";
+        //     y.innerHTML = 'Hide answer';
+        //   } 
+        // }
 
-        function toggleClock12() {
-          var x = document.getElementById("myDIV12");
-          var y = document.getElementById('clockButton12');
-          var z = x.style.display;
+        // function toggleClock12() {
+        //   var x = document.getElementById("myDIV12");
+        //   var y = document.getElementById('clockButton12');
+        //   var z = x.style.display;
 
-          if (z == "block") {
-            x.style.display = "none";
-            y.innerHTML = 'Show answer';
-          } 
+        //   if (z == "block") {
+        //     x.style.display = "none";
+        //     y.innerHTML = 'Show answer';
+        //   } 
 
-          else {
-            x.style.display = "block";
-            y.innerHTML = 'Hide answer';
-          } 
-        }
+        //   else {
+        //     x.style.display = "block";
+        //     y.innerHTML = 'Hide answer';
+        //   } 
+        // }
 
-        function toggleClock13() {
-          var x = document.getElementById("myDIV13");
-          var y = document.getElementById('clockButton13');
-          var z = x.style.display;
+        // function toggleClock13() {
+        //   var x = document.getElementById("myDIV13");
+        //   var y = document.getElementById('clockButton13');
+        //   var z = x.style.display;
 
-          if (z == "block") {
-            x.style.display = "none";
-            y.innerHTML = 'Show answer';
-          } 
+        //   if (z == "block") {
+        //     x.style.display = "none";
+        //     y.innerHTML = 'Show answer';
+        //   } 
 
-          else {
-            x.style.display = "block";
-            y.innerHTML = 'Hide answer';
-          } 
-        }
+        //   else {
+        //     x.style.display = "block";
+        //     y.innerHTML = 'Hide answer';
+        //   } 
+        // }
 
-        function toggleClock14() {
-          var x = document.getElementById("myDIV14");
-          var y = document.getElementById('clockButton14');
-          var z = x.style.display;
+        // function toggleClock14() {
+        //   var x = document.getElementById("myDIV14");
+        //   var y = document.getElementById('clockButton14');
+        //   var z = x.style.display;
 
-          if (z == "block") {
-            x.style.display = "none";
-            y.innerHTML = 'Show answer';
-          } 
+        //   if (z == "block") {
+        //     x.style.display = "none";
+        //     y.innerHTML = 'Show answer';
+        //   } 
 
-          else {
-            x.style.display = "block";
-            y.innerHTML = 'Hide answer';
-          } 
-        }
+        //   else {
+        //     x.style.display = "block";
+        //     y.innerHTML = 'Hide answer';
+        //   } 
+        // }
 
-        function toggleClock15() {
-          var x = document.getElementById("myDIV15");
-          var y = document.getElementById('clockButton15');
-          var z = x.style.display;
+        // function toggleClock15() {
+        //   var x = document.getElementById("myDIV15");
+        //   var y = document.getElementById('clockButton15');
+        //   var z = x.style.display;
 
-          if (z == "block") {
-            x.style.display = "none";
-            y.innerHTML = 'Show answer';
-          } 
+        //   if (z == "block") {
+        //     x.style.display = "none";
+        //     y.innerHTML = 'Show answer';
+        //   } 
 
-          else {
-            x.style.display = "block";
-            y.innerHTML = 'Hide answer';
-          } 
-        }
+        //   else {
+        //     x.style.display = "block";
+        //     y.innerHTML = 'Hide answer';
+        //   } 
+        // }
 
-        function toggleClock16() {
-          var x = document.getElementById("myDIV16");
-          var y = document.getElementById('clockButton16');
-          var z = x.style.display;
+        // function toggleClock16() {
+        //   var x = document.getElementById("myDIV16");
+        //   var y = document.getElementById('clockButton16');
+        //   var z = x.style.display;
 
-          if (z == "block") {
-            x.style.display = "none";
-            y.innerHTML = 'Show answer';
-          } 
+        //   if (z == "block") {
+        //     x.style.display = "none";
+        //     y.innerHTML = 'Show answer';
+        //   } 
 
-          else {
-            x.style.display = "block";
-            y.innerHTML = 'Hide answer';
-          } 
-        }
+        //   else {
+        //     x.style.display = "block";
+        //     y.innerHTML = 'Hide answer';
+        //   } 
+        // }
 
